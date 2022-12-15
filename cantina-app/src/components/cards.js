5e79d9fa17fba837bbd2cdca6fcb336408f0723d
Leagian <lea.giangrosso@gmail.com>
2022-12-15 19:40:48 +0100

    add page tatooine

---
+++ cantina-app/src/components/cards.js
@@ -0,0 +1,24 @@
+import 'cards.css'
+import Modal from 'react-modal';
+
+
+const cardImage = (props) => {
+
+	return (
+        <img src="#" onChange={() => this.setState({showModal: true})}/>
+	)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <p>Jabba Desilijic Tiure, plus couramment désigné comme Jabba le Hutt, était un seigneur du crime Hutt basé sur la planète Tatooine où il dirigeait l'une des plus puissantes organisations criminelles appelée le Clan Hutt. Il mourut étranglé par Leia Organa dans sa barge à voiles, la Khetanna en 4 ap.BY. </p>
---
+++ cantina-app/src/screens/charactersTatooine.js
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react"
+
+
+const Characters = () => {
+    const [charactersInfo, setCharactersInfo] = useState([]);
+    useEffect(() => {
+
+        fetch("https://miadil.github.io/starwars-api/api/all.json")
+			.then((res) => res.json())
+			.then((res) => console.log(res) || setCharactersInfo(res))
+	}, [])
+
+    return (
+		<div className="charactersBlock">
+ 			{charactersInfo.filter((elem) => elem.homeworld === "tatooine")
+			.map((elem, index) => {
+				
+				return (
+					<div>
+						<img
+							className="charac-img"
+							src={elem.image}
+							alt={elem.name}
+						/>
+						<p>{elem.name}</p>
+					</div>
+				)
+				})}
+		</div>
+	)
+}
+export default Characters
