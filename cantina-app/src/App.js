e5ee4087b925445214277d6e03e7786dd748d862
elyayanami <emmalavigne01@outlook.fr>
2022-12-16 01:46:17 +0100

    MAJ Header/Description

--- cantina-app/src/App.js
+++ cantina-app/src/App.js
@@ -12,6 +12,8 @@ function App() {
 
       <Header />
 
+      <Description />
+
       <Characters />
 
       <Routes>
--- cantina-app/src/components/description.js
+++ cantina-app/src/components/description.js
@@ -4,25 +4,27 @@ const Description = () => {
 
     return (
         <description className="App-description">
+<div id="description">   
+<p>
+            Tatooine était un monde désertique situé dans le secteur Arkanis de la Territoires de la Bordure Extérieure.<br/><br/>
+            Des fossiles laissaient penser aux scientifiques que Tatooine avait été recouverte par un océan à l'instar de Kamino.<br/>
 
-            <p>
-            Tatooine était un monde désertique situé dans le secteur Arkanis de la Territoires de la Bordure Extérieure. 
-            Des fossiles laissaient penser aux scientifiques que Tatooine avait été recouverte par un océan à l'instar de Kamino.
-
-            La planète fut colonisée par des corporations minières qui pensaient que la planète était riche en minerais. 
-            Cependant, lorsqu'elles découvrirent que ce n'était pas le cas, ces corporations plièrent bagages et laissèrent leurs ouvriers sur la planète. Depuis, Tatooine est devenu le repaire des hors-la-loi et le fief du Hutt Jabba Desilijic Tiure.
+            La planète fut colonisée par des corporations minières qui pensaient que la planète était riche en minerais. <br/><br/>
+            Cependant, lorsqu'elles découvrirent que ce n'était pas le cas, ces corporations plièrent bagages et laissèrent leurs ouvriers sur la planète. Depuis, Tatooine est devenu le repaire des hors-la-loi et le fief du Hutt Jabba Desilijic Tiure.<br/>
 
             La planète est également devenue connue pour ses courses de modules comme la Classique de Boonta Eve. Les Jawa, un peuple de ferrailleur, et les Tusken, un peuple de guerriers sanguinaires, étaient des espèces originaires de la planète. 
             </p>
+            </div>
+           
         
-            <a
+            {/* <a
 				className="App-link"
 				href="https://reactjs.org"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
 				Learn React
-			</a>
+			</a> */}
 
         </description>
 
